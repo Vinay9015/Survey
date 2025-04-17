8a0bdb704a5c1cd2da3b1a414a5b8b0f816c5bb1
yxt1220 <xyu67@hawk.iit.edu>
2025-04-17 17:31:33 -0500

    change password

--- db_config.py
+++ db_config.py
@@ -4,6 +4,6 @@ def get_connection():
     return mysql.connector.connect(
         host='localhost',
         user='root',
-        password='Kongcanyouhearme1?',
+        password='Password',
         database='student_survay'
     )
